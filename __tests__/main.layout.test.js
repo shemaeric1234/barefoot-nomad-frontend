07e91c19a0c8c0ab511f4a4b9716193b0c0d38ca
Nsengimana Veda Dominique <nsengimanavedadom@gmail.com>
2020-02-04 16:05:10 +0200

    chore(layout): implementing nested routes[Finishes #171074504]

---
+++ __tests__/main.layout.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import MainLayout from '../src/layouts/main.layout.jsx';
+
+describe('Render main layout', () => {
+    it('should render main layout successfully', () => {
+        const wrapper = mount(<MainLayout/>);
+        expect(wrapper.find('div').text()).toEqual('MainLayout');
+    })
+})
---
+++ src/views/signin.view.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+class Signin extends PureComponent {
+    render() {
+        return (
+            <div>Signin</div> 
+        )
+    }
+}
+
+export default Signin;
